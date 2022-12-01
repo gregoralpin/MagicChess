2b8184886574e9171743a09ec331196056dd4082
Stefano Pereira <stefano.pereira@transferoswiss.ch>
2022-12-01 19:48:46 -0300

    commented to run on simulation

--- Chess.cpp
+++ Chess.cpp
@@ -1,18 +1,18 @@
-#include "Board.h"
-#include "MoveManager.h"
-#include "Piece.h"
+// #include "Board.h"
+// #include "MoveManager.h"
+// #include "Piece.h"
 
-int main() {
-    Board* board = Board::GetBoardInstance();
-    board->mockUpgradePawnBoard();
-    board->printBoard();
+// int main() {
+//     Board* board = Board::GetBoardInstance();
+//     board->mockUpgradePawnBoard();
+//     board->printBoard();
     
-    MoveManager * moveManager = MoveManager::GetMoveManagerInstance();
+//     MoveManager * moveManager = MoveManager::GetMoveManagerInstance();
 
-    board->movePiece(6,7);
-    board->movePiece(57,56);
-    board->printBoard();
+//     board->movePiece(6,7);
+//     board->movePiece(57,56);
+//     board->printBoard();
 
-    // std::cout << "Is Check: " << isCheck << std::endl;
-    return 0;
-}
+//     // std::cout << "Is Check: " << isCheck << std::endl;
+//     return 0;
+// }
