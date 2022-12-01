2521b0cbfd426f40543c839fcb0f1348e8e80420
Stefano Pereira <stefano.pereira@transferoswiss.ch>
2022-12-01 19:50:02 -0300

    renamed methods

--- ChessDisplay.cpp
+++ ChessDisplay.cpp
@@ -7,7 +7,7 @@ void ChessDisplay::display_message_at(String message, int column, int row){
   print(message);
 }
 
-void ChessDisplay::lcd_print_top_left(String message){
+void ChessDisplay::print_top_left(String message){
   if(message.length() > 7){
     Serial.println("Message too big to be written at position");
     return;
@@ -16,7 +16,7 @@ void ChessDisplay::lcd_print_top_left(String message){
   display_message_at(message, 0, 0);
 }
 
-void ChessDisplay::lcd_print_top_right(String message){
+void ChessDisplay::print_top_right(String message){
   if(message.length() > 7){
     Serial.println("Message too big to be written at position");
     return;
@@ -25,7 +25,7 @@ void ChessDisplay::lcd_print_top_right(String message){
   display_message_at(message, LCD_COLUMNS - message.length(), 0);
 }
 
-void ChessDisplay::lcd_print_middle(String message){
+void ChessDisplay::print_middle(String message){
   if(message.length() > 20){
     Serial.println("Message too big to be written at position");
     return;
@@ -34,11 +34,19 @@ void ChessDisplay::lcd_print_middle(String message){
   display_message_at(message, 0, 2);
 }
 
-void ChessDisplay::lcd_print_bottom(String message){
+void ChessDisplay::clear_middle(){
+  display_message_at("                   ", 0, 2);
+}
+
+void ChessDisplay::print_bottom(String message){
   if(message.length() > 20){
     Serial.println("Message too big to be written at position");
     return;
   }
   
   display_message_at(message, 0, 3);
+}
+
+void ChessDisplay::clear_bottom(){
+  display_message_at("                   ", 0, 3);
 }
--- ChessDisplay.h
+++ ChessDisplay.h
@@ -7,8 +7,10 @@ class ChessDisplay : public LiquidCrystal
     using LiquidCrystal::LiquidCrystal;
     
     void display_message_at(String message, int column, int row);
-    void lcd_print_top_left(String message);
-    void lcd_print_top_right(String message);
-    void lcd_print_middle(String message);
-    void lcd_print_bottom(String message);
+    void print_top_left(String message);
+    void print_top_right(String message);
+    void print_middle(String message);
+    void clear_middle();
+    void print_bottom(String message);
+    void clear_bottom();
 };
